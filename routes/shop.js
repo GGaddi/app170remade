3b7e12cb1ad9bdc7d74aac25e37a4345c955332b
Alicia Ning <alicia.ning@gmail.com>
2016-02-12 03:23:17 +0000

    implemented shop listing through json

--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,34 @@
 // Get all of our friend data
-var accountData = require('../accounts.json');
+//var accountData = require('../accounts.json');
 
 exports.view = function(req, res){
-	console.log(accountData);
-	res.render('shop', accountData);
+	//console.log(accountData);
+	res.render('shop', {
+		'shop-items': [
+			{
+				'name': 'red shirt',
+				'id': '001r',
+				'lvl': 1,
+				'price': 0,
+				'img': 'http://placehold.it/105x105',
+				'description': 'the perfect shirt for a new adventurer with no money.'			
+			},
+			{
+				'name': 'blue shirt',
+				'id': '001b',
+				'lvl': 1,
+				'price': 0,
+				'img': 'http://placehold.it/105x105',
+				'description': 'the perfect shirt for a new adventurer with no money.'			
+			},
+			{
+				'name': 'green shirt',
+				'id': '001g',
+				'lvl': 1,
+				'price': 0,
+				'img': 'http://placehold.it/105x105',
+				'description': 'the perfect shirt for a new adventurer with no money.'			
+			}
+		]
+	});
 };
