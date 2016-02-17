5c8d722492262700c9ca4750594e680ee5b7cc37
Gabriel Gaddi <ggaddi@ucsd.edu>
2016-02-16 18:26:12 -0800

    Added character names

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,19 @@ var accountData = require('../accounts.json');
 exports.view = function(req, res){
 	console.log(req.query);
 	console.log(accountData);
+	var id = accountData['accounts'].length +1;
+	//console.log(id);
 	var username = req.query.username;
 	var password =  req.query.password;
 	var email = req.query.email;
+	var message = "";
 	//console.log(username);
 	var newAccount = {
+		"id": id,
 		"username": req.query.username,
 		"password": req.query.password,
-		"email": req.query.email
+		"email": req.query.email,
+		"message": message
 	//$("#submitBtn")
 	//console.log(data);
 	};
