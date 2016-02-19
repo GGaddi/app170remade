6abd75bdc3e79ba167c6247088b159a149b5ee78
Alicia Ning <alicia.ning@gmail.com>
2016-02-19 04:53:07 +0000

    friend page buttons

---
+++ routes/checkout.js
@@ -0,0 +1,8 @@
+// Get all of our friend data
+
+var accountData = require('../accounts.json');
+
+exports.view = function(req, res){
+	console.log(accountData);
+	res.render('checkout', accountData);
+};
