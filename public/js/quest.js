6f2a08853df966b8d521b7ab6948f3d24c323886
Michelle Nguyen <mqn008@gmail.com>
2016-02-19 00:52:07 -0800

    join page functional

--- public/js/quest.js
+++ public/js/quest.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 
 function initializePage() {
 	$("#begin").click(beginAdventure);
+	$("#start").click(beginGroupAdventure);
+	$('p').click(chooseAdventure);
 }
 
 function beginAdventure(e){
@@ -12,4 +14,18 @@ function beginAdventure(e){
 	var seconds = (hours*60*60) + (mins*60);
 	console.log(seconds);
     localStorage.setItem("time", seconds);
+}
+
+function beginGroupAdventure(e){
+	var hours = $("#boss-hr").text();
+	var mins = $("#boss-min").text();
+	var seconds = (hours*60*60) + (mins*60);
+	console.log(seconds);
+	localStorage.setItem("time", seconds);
+}
+
+function chooseAdventure(e){
+	$('#start').attr("onclick","location.href = 'adventure';")
+	$('p').removeClass("choose");
+	$(this).addClass("choose");
 }
